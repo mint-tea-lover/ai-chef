b530d438f20024d309765e6746224bb70d39e4d8
Azerchai <sinitsynad4@yandex.ru>
2026-01-03 18:51:12 +0300

    feat: handle form submit event and add new ingredient to the list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,41 @@
 import "./Main.css";
+import React from "react";
 
 export default function Main() {
+  const [ingredients, setIngredients] = React.useState([
+    "Chicken",
+    "Tomatoes",
+    "Oregano",
+  ]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    if (!ingredients.includes(newIngredient)) {
+      setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+    }
+    event.currentTarget.reset();
+  }
   return (
     <main>
-      <form className="ingredient-form">
+      <form className="ingredient-form" onSubmit={handleSubmit}>
         <input
           className="ingredient-inp"
           aria-label="Add ingredient"
           type="text"
           placeholder="e.g. milk"
-          id="ingredient-inp"
+          name="ingredient"
         />
         <button className="ingredient-add-btn" type="submit">
           + Add ingredient
         </button>
       </form>
-      dsgdf
+      <ul className="ingredients-list">
+        {ingredients.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
     </main>
   );
 }
