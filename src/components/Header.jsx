c888e46ee2dc42840aeea0158fd60a0e5c873b4c
Azerchai <sinitsynad4@yandex.ru>
2026-01-08 12:12:12 +0300

    chore: rename project and update header and package.json

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="header">
       <img className="header-logo" src="icons/chef_hat.svg" />
-      <h1 className="header-title">Chef Claude</h1>
+      <h1 className="header-title">AI Chef</h1>
     </header>
   );
 }
